d3d6b2b2eae8b415db6d8f58b51aa23ba03641a3
Иван Бурков <chessplayerkms2043@gmail.com>
2018-08-30 02:16:19 +0500

    fix

---
+++ APIClient/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace APIClient
+{
+    public class Client
+    {
+        protected const string ApiLink = "http://shpora.skbkontur.ru:80";
+        private readonly HttpClient client = new HttpClient();
+        private readonly RequestParameters requestParameters;
+
+        public Client(RequestParameters requestParameters)
+        {
+            this.requestParameters = requestParameters;
+        }
+
+        public async Task StartGame()
+        {
+            var url = ApiLink + "/task/game/start";
+            var response = await client.PostAsync(url, requestParameters.Content);
+        }
+
+        public async Task EndGame()
+        {
+            var url = ApiLink + "/task/game/finish";
+            var response = await client.PostAsync(url, requestParameters.Content);
+        }
+
+        public async Task GetStats()
+        {
+            var url = ApiLink + "/task/game/stats";
+            var response = await client.GetAsync(url, requestParameters.Content);
+        }
+    }
+}
---
+++ APIClient/RequestParameters.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace APIClient
+{
+    public class RequestParameters
+    {
+        private Dictionary<string, string> parameters { get; set; }
+
+        public RequestParameters()
+        {
+            parameters["token"] = "32553a4b-e29f-4d77-88e0-383dad817ef2";
+        }
+
+        public FormUrlEncodedContent Content => new FormUrlEncodedContent(parameters);
+    }
+}
